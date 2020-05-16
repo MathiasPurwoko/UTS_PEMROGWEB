617e8f44b0a148b56421ebff77effd8c3b815177
MathiasPurwoko <mathiaspurwoko14@outlook.com>
2020-05-16 19:54:16 +0700

    registrasi berhasil

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+var morgan = require('morgan');
+const app = express();
+
+
 //parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 //panggil routes
 var routes = require('./routes');
 routes(app);
+
+//daftarkan menu routes dari index
+app.use('/auth', require('./middleware'));
 
 
 app.listen(3000, () => {
