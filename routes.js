f46350e5a15b74c4a0484a3d8c9824c511f341c9
MathiasPurwoko <mathiaspurwoko14@outlook.com>
2020-05-16 18:49:23 +0700

    berhasil meghapus data sparepart berdasarkan id

--- routes.js
+++ routes.js
@@ -33,4 +33,7 @@ module.exports = function(app) {
     app.route('/hapusmontir')
         .delete(jsonku.hapusMontir);
 
+    app.route('/hapussparepart')
+        .delete(jsonku.hapusSparepart);
+
 } 
