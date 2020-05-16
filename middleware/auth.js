d59065e37d269d217bb8ab913fa0c5444d22bf54
MathiasPurwoko <mathiaspurwoko14@outlook.com>
2020-05-16 20:08:56 +0700

    login user

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,44 +1,97 @@
 var connection = require('../koneksi');
 var mysql = require('mysql');
-var md5 = require('MD5');
-var response = require('../res')
+var md5 = require('md5');
+var response = require('../res');
 var jwt = require('jsonwebtoken');
-var config = require('../controller')
 var config = require('../config/secret');
 var ip = require('ip');
 
 //controller untuk register
-exports.registrasi = function(req,res) {
+exports.registrasi = function(req,res){
     var post = {
-        nama_user: req.body.nama_user,
+        username: req.body.username,
         email: req.body.email,
         password: md5(req.body.password),
-        level: req.body.level
+        role: req.body.role,
+        tanggal_daftar: new Date()
     }
 
     var query = "SELECT email FROM ?? WHERE ??=?";
-    var table = ["t_user", "email", post.email];
+    var table = ["t_user","email", post.email];
 
     query = mysql.format(query,table);
 
     connection.query(query, function(error, rows) {
         if(error){
             console.log(error);
+
         }else {
             if(rows.length == 0){
                 var query = "INSERT INTO ?? SET ?";
                 var table = ["t_user"];
-                query = mysql.format(query, table);
+                query = mysql.format(query,table);
                 connection.query(query, post, function(error, rows){
                     if(error){
                         console.log(error);
+                        
                     }else {
-                        response.ok("Berhasil menambahkan user baru", res);
+                        response.ok("Berhasil Menambahkan data user baru",res);
+
                     }
                 });
             }else {
-                response.ok("Email sudah terdaftar!",res);
+                response.ok("Email sudah tedaftar!",res);
             }
         }
     })
-} 
+}
+
+//controller untuk login
+exports.login = function (req, res) {
+    var post = {
+        password: req.body.password,
+        email: req.body.email
+    }
+
+    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+    var table = ["t_user", "password", md5(post.password), "email", post.email];
+
+    query = mysql.format(query, table);
+    connection.query(query, function (error, rows) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 1) {
+                var token = jwt.sign({ rows }, config.secret, {
+                    expiresIn: 1440
+                });
+                id_user = rows[0].id_user;
+
+                var data = {
+                    id_user: id_user,
+                    access_token: token,
+                    ip_address: ip.address()
+                }
+
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["access_token"];
+
+                query = mysql.format(query, table);
+                connection.query(query, data, function (error, rows) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        res.json({
+                            success: true,
+                            message: "Token JWT Tergenerate!",
+                            token: token,
+                            currUser: data.id_user
+                        });
+                    }
+                });
+            } else {
+                res.json({ "Error": true, "Message": "Email atau password salah!" });
+            }
+        }
+    });
+}
