29403dc238daf5a082c7a7021f54f5641238eb5f
MathiasPurwoko <mathiaspurwoko14@outlook.com>
2020-05-16 18:21:58 +0700

    berhasil menampilkan data sparepart berdasarkan id

--- controller.js
+++ controller.js
@@ -42,3 +42,16 @@ exports.tampilberdasarkanidmontir = function (req, res) {
             }
         });
 };
+
+//menampilkan semua data sparepart berdasarkan id
+exports.tampilberdasarkanidsparepart = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_sparepart WHERE id_sparepart = ?', [id],
+        function(error, rows, fields) {
+            if(error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res)
+            }
+        });
+};
--- routes.js
+++ routes.js
@@ -11,8 +11,11 @@ module.exports = function(app) {
 
     app.route('/tampilsparepart')
         .get(jsonku.tampilsemuasparepart);
-        
+
     app.route('/tampilmontir/:id')
         .get(jsonku.tampilberdasarkanidmontir);
+
+    app.route('/tampilsparepart/:id')
+        .get(jsonku.tampilberdasarkanidsparepart);
 
 } 
